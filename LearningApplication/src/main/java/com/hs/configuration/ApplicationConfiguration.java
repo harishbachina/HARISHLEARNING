4f43f8c83276285fa99feac6d46e316d24c4dceb
harishbachina <hareesh.bachina@gmail.com>
2017-12-16 17:09:38 +0530

    updating

--- LearningApplication/src/main/java/com/hs/configuration/ApplicationConfiguration.java
+++ LearningApplication/src/main/java/com/hs/configuration/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.hs.configuration;
 import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -19,6 +20,7 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
 		view.setPrefix("/WEB-INF/pages/");
 		view.setSuffix(".jsp");
 		registry.viewResolver(view);
+	
 	}
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
