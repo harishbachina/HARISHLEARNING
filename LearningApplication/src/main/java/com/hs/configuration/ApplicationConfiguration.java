7520c142f97d483f88f6da6a1d84afdf544b18da
harishbachina <hareesh.bachina@gmail.com>
2017-12-16 16:26:13 +0530

    removing web.xml

--- LearningApplication/src/main/java/com/hs/configuration/ApplicationConfiguration.java
+++ LearningApplication/src/main/java/com/hs/configuration/ApplicationConfiguration.java
@@ -1,5 +1,29 @@
 package com.hs.configuration;
 
-public class ApplicationConfiguration {
+import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.hs")
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver view=new InternalResourceViewResolver();
+		view.setPrefix("/WEB-INF/pages/");
+		view.setSuffix(".jsp");
+		registry.viewResolver(view);
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+	
+	
 }
